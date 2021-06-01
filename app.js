4fb36a8a4711be3956824693666bf9e9b4426ada
Lawrence <lawrencematsuoka@gmail.com>
2021-06-01 12:54:00 -0300

    second machine

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Hello Docker!");
+
+// C:\Users\Lawre\Documents\testJs>docker build -t hello-docker .
+// File path, docker command, build, -t means somethin, and hello-docker is the file, the period signifies using the current filepath
